f5f127af6d9303c325192c1bee5aa73a0f26671a
novaprospectCAU <novaprospect110a@gmail.com>
2024-02-16 22:45:36 +0900

    리스트 아이템을 모두 arrowJS 스타일로 추가하였습니다

--- src/todo-list-item.js
+++ src/todo-list-item.js
@@ -1,4 +1,4 @@
-import { html } from "@arrow-js/core";
+import { html } from "https://esm.sh/@arrow-js/core";
 
 export class TodoListItem {
   constructor(list, store, item, updateAll) {
@@ -50,45 +50,33 @@ export class TodoListItem {
     const newListItem = document.createElement("li");
     newListItem.classList.add("todo-list__item");
 
-    //좌측 공간(체크 버튼, 텍스트 필드, input필드)
-    const newListItemLeft = document.createElement("div");
-    newListItemLeft.classList.add("todo-list__item-left");
+    const initTemplate = html` <div class="todo-list__item-left">
+        <button class="todo-list__item-check-button"></button>
+        <div class="todo-list__item-text">${this.item.text}</div>
+        <input class="todo-list__item-input todo-list--switch" type="text" />
+      </div>
+      <button class="todo-list__delete-button">X</button>`;
 
-    //체크 버튼
-    const newListItemCheckButton = document.createElement("button");
-    newListItemCheckButton.classList.add("todo-list__item-check-button");
+    initTemplate(newListItem);
 
-    //텍스트 필드
-    const newListItemText = document.createElement("div");
-    newListItemText.classList.add("todo-list__item-text");
-
-    //input필드(Default : off)
-    const newListItemInput = document.createElement("input");
-    newListItemInput.classList.add("todo-list__item-input");
-    newListItemInput.classList.add("todo-list--switch");
-
-    //삭제 버튼
-    const newListItemDeleteButton = document.createElement("button");
-    newListItemDeleteButton.classList.add("todo-list__delete-button");
-    newListItemDeleteButton.textContent = "X";
+    const newListItemDeleteButton = newListItem.querySelector(
+      ".todo-list__delete-button"
+    );
 
     newListItemDeleteButton.addEventListener("click", () => {
       this.store.items = this.store.items.filter(
         (currentItem) => currentItem !== this.item
       );
-      this.updateAll();
     });
+
+    const newListItemCheckButton = newListItem.querySelector(
+      ".todo-list__item-check-button"
+    );
 
     newListItemCheckButton.addEventListener("click", () => {
       this.item.isCompleted = !this.item.isCompleted;
       this.updateAll();
     });
-
-    newListItemLeft.append(newListItemCheckButton);
-    newListItemLeft.append(newListItemText);
-    newListItemLeft.append(newListItemInput);
-    newListItem.append(newListItemLeft);
-    newListItem.append(newListItemDeleteButton);
 
     this.list.append(newListItem);
     return newListItem;
