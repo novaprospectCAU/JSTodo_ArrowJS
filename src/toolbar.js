60a5b612428238f20ad0dd78ba2646388205b2a9
novaprospectCAU <novaprospect110a@gmail.com>
2024-02-17 20:15:36 +0900

    주석 제거했습니다

--- src/toolbar.js
+++ src/toolbar.js
@@ -1,33 +1,6 @@
 import { reactive, html } from "https://esm.sh/@arrow-js/core";
 import { store } from "./store.js";
 
-// import { Counter } from "./counter.js";
-// import { Option } from "./option.js";
-// import { ClearButton } from "./clear-button.js";
-
-// export class Toolbar {
-//   constructor(root, store, updateAll) {
-//     this.toolbar = root.querySelector(".todo-list__menu");
-
-//     this.store = store;
-
-//     this.counter = new Counter(root, store, updateAll);
-//     this.option = new Option(root, store, updateAll);
-//     this.clearButton = new ClearButton(root, store, updateAll);
-//   }
-
-//   update() {
-//     if (this.store.items.length > 0) {
-//       this.toolbar.classList.remove("todo-list__menu--hiding");
-//     } else {
-//       this.toolbar.classList.add("todo-list__menu--hiding");
-//     }
-//     this.counter.update();
-//     this.option.update();
-//     this.clearButton.update();
-//   }
-// }
-
 export const toolbar = html` <div
   class="${() =>
     `todo-list__menu ${
